5993b02ce709335960cbf65743c4a65504a48b1f
Samuel Cattini-Schultz <catts16@gmail.com>
2018-02-15 14:08:29 +1100

    Set logging level to INFO

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import collections
 import time
 
 logger = logging.getLogger("discord")
-logger.setLevel(logging.WARNING)
+logger.setLevel(logging.INFO)
 handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
 handler.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s"))
 logger.addHandler(handler)
